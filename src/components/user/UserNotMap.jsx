8b1ed6abb791d9a8221d545e6618b0898aa69888
Bassaamm <Bassamalqrni@gmail.com>
2023-08-22 21:02:56 +0300

    first commit

--- src/components/user/UserNotMap.jsx
+++ src/components/user/UserNotMap.jsx
@@ -1,21 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./UserNotMap.module.css";
 import { logOutApiAuth } from "../../service/supabaseAuthAPI";
-import { useMemo } from "react";
+import { useEffect } from "react";
 import { useUser } from "../../auth/useUser";
 
 function UserNotMap() {
   const navigate = useNavigate();
   const { isAuthenticated } = useUser();
-  const useState = useMemo(() => {
-    navigate(0);
+  useEffect(() => {
+    navigate("/");
   }, [isAuthenticated]);
-
+  function handleClick() {
+    logOutApiAuth();
+    navigate("/");
+  }
   return (
     <div className={styles.user}>
       <img src="\download.png" alt="avatar" />
       <span>Welcome, Bassam</span>
-      <button onClick={logOutApiAuth}>Logout</button>
+      <button onClick={handleClick}>Logout</button>
     </div>
   );
 }
